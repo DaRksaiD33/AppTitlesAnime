1ec58c3c8e00ff31c7dcf62f96256cfde539f18e
Назаров Михаил <niseshot02@gmail.com>
2025-04-10 16:01:22 +0500

    добавил форму

---
+++ AppTitlesAnime/FormMain.cs
@@ -0,0 +1,15 @@
+namespace AppTitlesAnime
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnShowTypes_Click(object sender, EventArgs e)
+        {
+            FormListTypes formListTypes = new FormListTypes();
+        }
+    }
+}
